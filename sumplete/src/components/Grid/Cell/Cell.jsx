4ecc6ccd9bb146958b2a13ca5fafbd45fcd2829e
lovretomic <tomiclovre05@gmail.com>
2023-03-23 11:17:58 +0100

    Added marking cells

--- sumplete/src/components/Grid/Cell/Cell.jsx
+++ sumplete/src/components/Grid/Cell/Cell.jsx
@@ -1,6 +1,34 @@
-const Cell = ({ value, type }) => {
+import { cellStatus } from "../../../constants/enums";
+
+const Cell = ({ value, type, id, status, setGrid }) => {
+  const handleCellStatusSwitch = (id) => {
+    setGrid((prevGrid) => {
+      const newGrid = [...prevGrid];
+      const cell = newGrid.find((cell) => cell.id === id);
+
+      let newStatus;
+      switch (cell.status) {
+        case cellStatus.none:
+          newStatus = cellStatus.selected;
+          break;
+        case cellStatus.selected:
+          newStatus = cellStatus.removed;
+          break;
+        default:
+          newStatus = cellStatus.none;
+          break;
+      }
+
+      cell.status = newStatus;
+      return newGrid;
+    });
+  };
+
   return (
-    <div className={`cell ${type}`}>
+    <div
+      className={`cell ${type} ${status}`}
+      onClick={() => handleCellStatusSwitch(id)}
+    >
       <p className="cell__content">{value}</p>
     </div>
   );
--- sumplete/src/components/Grid/Grid.jsx
+++ sumplete/src/components/Grid/Grid.jsx
@@ -1,13 +1,21 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import { Helper } from "../../Helper";
 import Cell from "./Cell";
 
 const Grid = () => {
-  const grid = Helper.generateGrid(3);
+  const [grid, setGrid] = useState(Helper.generateGrid(3));
+
   return (
     <div className="grid">
       {grid.map((cell) => (
-        <Cell value={cell.value} type={cell.type} key={cell.id} />
+        <Cell
+          value={cell.value}
+          type={cell.type}
+          id={cell.id}
+          status={cell.status}
+          setGrid={setGrid}
+          key={cell.id}
+        />
       ))}
     </div>
   );
